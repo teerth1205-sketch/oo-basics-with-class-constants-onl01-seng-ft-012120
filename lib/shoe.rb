195e207161eb9aac4befe46ef8293fad6ae5f999
Teerth Patel <teerth1205@gmail.com>
2020-01-29 23:30:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-    BRANDS.uniq!
+    if !(BRANDS.include?(@brand))
+       BRANDS << brand
+     end 
+    
   
   end
 
